d3570f8efb9f3ecece77ba8add67002e475adc52
dyp <duan.yapeng@meteodyn.com>
2020-04-23 09:53:42 +0800

    a

--- duan.py
+++ duan.py
@@ -12,6 +12,8 @@
         必须建立　/data/db/journal文件夹
         日志在　/var/log/mongodb/mongod.log
     redis-server.conf
+        需要建立　/var/lib/redis，这是运行目录，生成dump.rdb
+        运行时要导入redis.conf,关闭其中点守护模式　daemonize no
 
 6. 从web14到web20，添加了数据库，添加了redis，添加了ngix，保留了wsgi，
     服务器部署从windows上的能跑通变为了
